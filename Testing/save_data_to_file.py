ba096bc9e0d62e3754dea30abd3cc2da96c5ff0b
Thomas Keady <thomas.keady@jhu.edu>
2016-01-19 18:42:20 -0500

    Create save_data_to_file.py
    Comments at top tell you how to get it back

---
+++ Testing/save_data_to_file.py
@@ -0,0 +1,56 @@
+# How to grab our data and put it into a .npy file
+
+# To load it back in:
+# 
+# import numpy as np
+# 
+# membrane_images = np.load('our_data.npy')
+# 
+
+import ndio
+import ndio.remote.OCP as OCP
+oo = OCP()
+
+import ndio.remote.OCPMeta as NDLIMS
+nn = NDLIMS()
+
+import ndio.convert.png as ndpng	# For export to pngs later
+
+import numpy as np
+
+file = open('our_data.txt', 'w')	# Open file
+
+print "Done importing\n"
+
+tokens = oo.get_public_tokens()
+
+image_token = 'kasthuri11cc'
+annotation_token = 'cv_kasthuri11_membrane_2014'
+
+# Get channel ROI for token
+channel_ROI = nn.get_metadata('cv_kasthuri11_membrane_2014')['channels']
+
+# Get membrane ROI coordinates
+membrane_group_ROI = channel_ROI['image']['rois']['ac4']
+
+# Sets membrane query in Python
+membrane_query = {
+    'token': 'cv_kasthuri11_membrane_2014',
+    'channel': 'image',
+    'x_start': membrane_group_ROI['x'][0],
+    'x_stop': membrane_group_ROI['x'][1],
+    'y_start': membrane_group_ROI['y'][0],
+    'y_stop': membrane_group_ROI['y'][1],
+    'z_start': membrane_group_ROI['z'][0],
+    'z_stop': membrane_group_ROI['z'][1],
+    'resolution': membrane_group_ROI['resolution'],
+}
+
+print "Getting images\n"
+membrane_images = oo.get_cutout(**membrane_query)
+
+print "Printing to file\n"
+np.save('our_data.npy', membrane_images)
+
+print "Done."
+
